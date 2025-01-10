e3b2de5e241645f12ca37602f21839bab581d5b4
Preetham Sagar <borem.preetham@bizacuity.com>
2025-01-10 15:20:34 +0530

    added delete and get all activities apis

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
     console.log(`server started at port ${port}`)
 })
 
-app.post("/activity", async (req, res) => {
+app.post("/api/activity", async (req, res) => {
     const activity = req.body;
 
     if (!activity.claimNumber || !activity.claimDate || !activity.activityDate || !activity.sportsActivity || !activity.courtFees || !activity.bookedBy || !activity.invoiceNo || !activity.slotStartTime || !activity.slotEndTime || !activity.courtsBooked || !activity.employeesImageURL || !activity.employeesPlayed || !activity.employeesCount) {
@@ -25,6 +25,28 @@ app.post("/activity", async (req, res) => {
         res.status(201).json({ success: true, data: newActivity });
     } catch (error) {
         console.error("Error in Creating Activity:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
+
+app.delete("/api/activity/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log("Received Id for deleting activity: ", id);
+    try {
+        await Activity.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: `product deleted ${id}` });
+    } catch (error) {
+        console.error("Error in Deleting Activity: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
+app.get("/api/activities", async (req, res) => {
+    try {
+        const activities = await Activity.find({});
+        res.status(200).json({ success: true, data: activities });
+    }
+    catch (error) {
+        console.log("Error in fetching all activities: ", error.message);
         res.status(500).json({ success: false, message: "Server Error" });
     }
 })
