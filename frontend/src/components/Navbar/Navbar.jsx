55c85ed67b3fead5ff6386a9c6d3499daffb4c10
Preetham Sagar <borem.preetham@bizacuity.com>
2025-01-16 12:00:50 +0530

    feat: front end setup

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Typography variant="h3" gutterBottom>
+        Hello World
+      </Typography>
+    </div>
+  )
+}
+
+export default Navbar
